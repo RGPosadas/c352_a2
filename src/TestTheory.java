84408a56788656ba1540abdaba024220d9e43ef6
RGPosadas <ritchelle.posadas@gmail.com>
2018-10-16 21:57:51 -0400

    testing queue methods

--- src/TestTheory.java
+++ src/TestTheory.java
@@ -15,9 +15,34 @@ public class TestTheory {
 	}
 	
 	//////////// QUEUE METHODS //////////////////
-//	public 
-	public static void findMultipleStack(int[] arr, int n) {
-		
+	public static void enqueue(int[] arr, int n) {
+		int freeIndex = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				freeIndex = i;
+				break;
+			}
+			i++;
+		}
+		System.out.println("Putting " + n + " in index " + freeIndex);
+		arr[freeIndex] = n;
+	}
+	
+	public static int dequeue(int[] arr, int index) {
+		int num = arr[index];
+		arr[index] = 0;
+		return num;
+	}
+	
+	public static int[] findMultipleStack(int[] arr, int n) {
+		int[] myQ = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % n == 0 && arr[i] != 0) {
+				System.out.println("Sending " + arr[i] + " to enqueue");
+				enqueue(myQ, arr[i]);
+			}
+		}
+		return myQ;
 	}
 
 	public static void main(String[] args) {
@@ -28,7 +53,13 @@ public class TestTheory {
 		findMultipleLoop(a, n);
 		
 		System.out.println("\n======== Using a stack ========");
+		int[] myStack = findMultipleStack(a,n);
 		
+		for (int i = 0; i < myStack.length; i++) {
+			System.out.println(myStack[i]);
+		}
+//		int[] random = new int[5];
+//		System.out.println(random[i]);
 	}
 
 }
